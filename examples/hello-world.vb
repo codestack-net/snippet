4ccd5f41c4b5aad9dd40311af55ba7a155be281d
CodeStack <codestack.net@gmail.com>
2018-04-09 22:01:34 +1000

    Added vb.net example

---
+++ examples/hello-world.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Namespace CodeStack
+
+    Class Program
+
+        Sub Main(ByVal args As String())
+            Console.WriteLine("Hello World!")
+        End Sub
+		
+    End Class
+	
+End Namespace
