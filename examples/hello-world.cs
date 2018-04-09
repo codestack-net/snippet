e150b71e682297f23b1efb3b0d12ec1f447c4341
CodeStack <codestack.net@gmail.com>
2018-04-09 21:51:34 +1000

    Added snippet service with example

---
+++ examples/hello-world.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CodeStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
